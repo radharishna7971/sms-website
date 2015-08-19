e92ab3a2170ec0afaf5025d9343013c2d9b6cc56
bdstein33 <bdstein33@gmail.com>
2015-08-19 16:51:43 -0700

    Remove semantic, edit deploy production file

--- deploy_production.sh
+++ deploy_production.sh
@@ -3,8 +3,9 @@
 echo COPYING CLIENT/SERVER FILES
 scp -r client/ root@107.170.195.138:smstudios-app
 scp -r server/ root@107.170.195.138:smstudios-app
+scp package.json root@107.170.195.138:smstudios-app
 echo TUNNELING
-ssh -u'root' -p$SSH_PASS -h107.170.195.138 <<'ENDSSH'
+ssh root@107.170.195.138 <<'ENDSSH'
 export NODE_ENV=production
 pm2 restart server
 ENDSSH
