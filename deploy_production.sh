617bddc2e0ad77d25c9ca914cd38f1c15c9982c7
bdstein33 <bdstein33@gmail.com>
2015-08-19 16:16:53 -0700

    Update deploy_production.sh

--- deploy_production.sh
+++ deploy_production.sh
@@ -4,7 +4,7 @@ echo COPYING CLIENT/SERVER FILES
 scp -r client/ root@107.170.195.138:smstudios-app
 scp -r server/ root@107.170.195.138:smstudios-app
 echo TUNNELING
-ssh root@107.170.195.138 <<'ENDSSH'
+ssh -u'root' -p$SSH_PASS -h107.170.195.138 <<'ENDSSH'
 export NODE_ENV=production
 pm2 restart server
 ENDSSH
