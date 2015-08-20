400087cdbfa35cea8ad150b3376ea839426f1a10
bdstein33 <bdstein33@gmail.com>
2015-08-19 17:05:40 -0700

    Fix server config files

--- deploy_production.sh
+++ deploy_production.sh
@@ -7,6 +7,6 @@ scp package.json root@107.170.195.138:smstudios-app
 echo TUNNELING
 ssh root@107.170.195.138 <<'ENDSSH'
 export NODE_ENV=production
-pm2 restart server
+pm2 restart server=
 ENDSSH
 echo COMPLETED
