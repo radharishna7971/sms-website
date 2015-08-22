bf01c5223b1376b0131596496d283172bc0c4f67
bdstein33 <bdstein33@gmail.com>
2015-08-21 17:46:40 -0700

    Fix # issue

--- server/routes/emailList.routes.js
+++ server/routes/emailList.routes.js
@@ -3,4 +3,9 @@
  module.exports = function(app) {
 
   app.post('/api/email/add', emailList.addEmail);
+
+  app.get('/login', function(req, res) {
+    console.log(app.redirect);
+    res.redirect('/#/login');
+  });
  };
---
+++ server/routes/redirect.routes.js
@@ -0,0 +1,8 @@
+
+ // module.exports = function(app) {
+
+ //  app.get('/login', function(req, res) {
+ //    console.log("YAY!!!")
+ //    res.redirect('/#/login');
+ //  });
+ // };
