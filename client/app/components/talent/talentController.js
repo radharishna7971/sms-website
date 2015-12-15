17d7f7897956d5e9723ae9e22c9b5daaf31de79f
unknown <AL1342@ALIPL0104.accionlabs.in>
2015-12-15 16:17:15 +0530

    Removed the decimal with k budget and box office income

--- client/app/components/talent/talentController.js
+++ client/app/components/talent/talentController.js
@@ -48,7 +48,7 @@
                     return (num / 1000000).toFixed(1).replace(/\.0$/, '') + 'M';
                 }
                 if (num >= 1000) {
-                    return (num / 1000).toFixed(1).replace(/\.0$/, '') + 'K';
+                    return (num / 1000).toFixed(0).replace(/\.0$/, '') + 'K';
                 }
                 return num;
             }
